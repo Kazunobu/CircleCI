cfe37b737972213d55b44a56430742ace862ed13
kazunobu <kdnb.com@gmail.com>
2018-08-18 15:54:45 +0900

    sample unittest

---
+++ test_calculations.py
@@ -0,0 +1,14 @@
+import unittest
+
+import calculations
+
+class CalculationsTest(unittest.TestCase):
+   """割り算のテスト"""
+   def test_divide_normal(self):
+      self.assertEqual(calculations.divide(2, 2), 1)
+
+   def test_divide_contain_zero(self):
+      self.assertEqual(calculations.divide(0, 1), 1)
+
+if __name__ == "__main__":
+   unittest.main()
